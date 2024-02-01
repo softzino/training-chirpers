4d5dbab4b9caf78861e583fc26a04af9075906db
Faisal Ahmed <foysal20x@gmail.com>
2024-02-01 13:30:57 +0600

    add frontend staff

---
+++ vue-frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="text-3xl font-bold underline">Hello Chirers</div>
+  </main>
+</template>
